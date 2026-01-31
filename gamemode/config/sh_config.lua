93a38751a8bf00b0c4f6a6a014e369a6945a925a
Noah Sabaj <noahsabaj@gmail.com>
2026-01-30 21:31:16 -0500

    fix(helix): update weaponRaiseTime to 0.3 for improved weapon handling

--- gamemode/config/sh_config.lua
+++ gamemode/config/sh_config.lua
@@ -186,7 +186,7 @@ end, {
 ix.config.Add("weaponAlwaysRaised", false, "Whether or not weapons are always raised.", nil, {
 	category = "server"
 })
-ix.config.Add("weaponRaiseTime", 0.25, "The time it takes for a weapon to raise.", nil, {
+ix.config.Add("weaponRaiseTime", 0.3, "The time it takes for a weapon to raise.", nil, {
 	data = {min = 0.1, max = 60, decimals = 1},
 	category = "server"
 })
